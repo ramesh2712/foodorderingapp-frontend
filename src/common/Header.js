c08c3a6aaa574d56e0577821c120f25e9126ca6b
Ramesh Prajapati <ramesh.prajapathi@asiglobal.co>
2019-03-24 00:42:50 +0530

    Added Login button with SVG Icon

--- src/common/Header.js
+++ src/common/Header.js
@@ -1,15 +1,21 @@
 import React, {Component} from 'react';
 import './Header.css';
 import Button from '@material-ui/core/Button';
+import { SvgIcon } from '@material-ui/core';
+import AccountCircle from '@material-ui/icons/AccountCircle';
 
 class Header extends Component {
+
     render(){
         return(
             <div>
                 <header className="app-header">
                     <div className="login-button">
-                         <Button variant="contained" color="default"> 
-                            Login 
+                         <Button variant="contained" color="default" > 
+                         <SvgIcon>
+                             <AccountCircle />
+                         </SvgIcon>
+                            <span className="login-spacing">Login</span> 
                          </Button>
                     </div>
                 </header>
