9e6fa1c5309fff7c317d0cb4b2b6e25ea2fe7e60
Ramesh Prajapati <ramesh.prajapathi@asiglobal.co>
2019-03-24 03:19:39 +0530

    Done with Search Bar inside Header

--- src/common/Header.js
+++ src/common/Header.js
@@ -1,16 +1,51 @@
 import React, {Component} from 'react';
 import './Header.css';
 import Button from '@material-ui/core/Button';
-import { SvgIcon } from '@material-ui/core';
+import { SvgIcon, withStyles } from '@material-ui/core';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import Fastfood from '@material-ui/icons/Fastfood';
+import Search from '@material-ui/icons/Search';
+import Input from '@material-ui/core/Input';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import PropTypes from 'prop-types';
 
+const styles = theme => ({
+    root: {
+        color: '#fff',
+        width: '320px',
+        fontSize: 15,
+        '&:after': {
+            // The MUI source seems to use this but it doesn't work
+            borderBottom: '1px solid white',
+          },
+      }
+})
 class Header extends Component {
 
     render(){
+        const { classes } = this.props;
+
         return(
             <div>
                 <header className="app-header">
+                    <div> 
+                         <SvgIcon className="app-logo">
+                             <Fastfood />
+                         </SvgIcon>
+                    </div>
+                    <div>
+                        <Input className={classes.root}
+                             id="input-with-icon-adornment"
+                             startAdornment={
+                                 <InputAdornment position="start" className="search">
+                                    <SvgIcon>
+                                        <Search />
+                                     </SvgIcon>
+                                </InputAdornment>
+                             }
+                             placeholder="Search by Restaurant Name"
+                         />
+                    </div>
                     <div className="login-button">
                          <Button variant="contained" color="default" > 
                          <SvgIcon>
@@ -19,15 +54,13 @@ class Header extends Component {
                             <span className="login-spacing">Login</span> 
                          </Button>
                     </div>
-                    <div> 
-                         <SvgIcon className="app-logo">
-                             <Fastfood />
-                         </SvgIcon>
-                    </div>
                 </header>
             </div>
         )
     }
 }
 
-export default Header;
+Header.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+export default withStyles(styles)(Header);
