e657b4964386214a08ae1f75e8d867b598a312ac
Ramesh Prajapati <ramesh.prajapathi@asiglobal.co>
2019-03-29 16:26:19 +0530

    Done with Card displaying Resturant with image and name , category

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,15 +1,94 @@
-import React , {Component} from 'react';
+import React, { Component } from 'react';
 import './Home.css';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 import Header from '../../common/header/Header';
+import Card from "@material-ui/core/Card";
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
 
-class Home extends Component{
-    render(){
+const styles = {
+    card: {
+      maxWidth: 300,
+      height: 'auto',
+    },
+    media: {
+      height: 140,
+      width: '100%',
+    },
+    cardContent: {
+        margin: 'auto',
+        width: 240,
+    }
+  };
+
+class Home extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            restaurantData: [],
+            filteredRestaurantData: [],
+            currentSearchValue: ""
+        }
+    }
+    componentWillMount() {
+        this.callApiToGetRestaurantData();
+    }
+    callApiToGetRestaurantData = () => {
+        //GET /restaurant
+        let xhrPosts = new XMLHttpRequest();
+        let that = this
+
+        xhrPosts.addEventListener("readystatechange", function () {
+            if (this.readyState === 4) {
+                console.log(JSON.parse(this.responseText));
+                let data = JSON.parse(this.responseText);
+                that.setState({
+                    restaurantData: data
+                })
+            }
+        });
+        xhrPosts.open("GET", this.props.baseUrl + "/restaurant");
+        xhrPosts.send();
+    }
+
+    render() {
+        const {classes} = this.props;
         return (
-              <div>
-                 <Header history={this.props.history}/>
-              </div>
+            <div>
+                <Header history={this.props.history} />
+                <div className="flex-container">
+                    {this.state.restaurantData.map(restaurant => (
+                        <Card className={this.props.card} key={restaurant.restaurants[0].id}>
+                            <CardActionArea>
+                                <CardMedia
+                                 className={classes.media}
+                                 image={restaurant.restaurants[0].photo_URL}
+                                 title={restaurant.restaurants[0].restaurant_name}
+                                />
+                                <CardContent className={classes.cardContent}>
+                                   <Typography gutterBottom variant="h5" component="h2">
+                                      {restaurant.restaurants[0].restaurant_name}
+                                   </Typography> <br/><br/>
+                                   <Typography component="p">
+                                     {restaurant.restaurants[0].categories}
+                                  </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    ))
+                    }
+                </div>
+            </div>
         )
     }
 }
 
-export default Home;
+Home.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Home);
