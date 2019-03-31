78dc60e185d7923a3b387a5797b32f9835530708
Ramesh Prajapati <ramesh.prajapathi@asiglobal.co>
2019-03-31 11:55:25 +0530

    Added Checkout button

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -7,12 +7,15 @@ import Header from '../../common/header/Header';
 import Star from '@material-ui/icons/Star';
 import Divider from '@material-ui/core/Divider';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircle, faLessThanEqual } from '@fortawesome/free-solid-svg-icons';
+import { faCircle } from '@fortawesome/free-solid-svg-icons';
 import IconButton from '@material-ui/core/IconButton';
 import Add from '@material-ui/icons/Add';
 import Card from "@material-ui/core/Card";
 import Snackbar from '@material-ui/core/Snackbar';
 import CloseIcon from '@material-ui/icons/Close';
+import { CardContent } from '@material-ui/core';
+import CardActions from '@material-ui/core/CardActions';
+import Button from '@material-ui/core/Button';
 
 const styles = {
     star: {
@@ -22,8 +25,12 @@ const styles = {
         margin: 10
     },
       card: {
-        width: '95%',
+        width: '90%',
+        padding: 10,
         height: 'auto'
+    },
+    button :{
+        width : '100%'
     }
 };
 
@@ -177,7 +184,16 @@ class Details extends Component {
                       }
                      </div>
                      <div className="cart-container">
-                         Test
+                         <Card className={classes.card}>
+                            <CardContent>
+                                
+                            </CardContent>
+                            <CardActions>
+                                <Button variant="contained" color="primary" className={classes.button}>
+                                    CHECKOUT
+                                </Button>
+                             </CardActions>
+                         </Card>
                      </div>
                 </div>
                 <Snackbar
