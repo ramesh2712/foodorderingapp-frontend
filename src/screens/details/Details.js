7e19cfbce22eeca0066a1b81b73983bbe51714b1
Ramesh Prajapati <ramesh.prajapathi@asiglobal.co>
2019-03-31 12:42:17 +0530

    Done with empty cart

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -11,11 +11,13 @@ import { faCircle } from '@fortawesome/free-solid-svg-icons';
 import IconButton from '@material-ui/core/IconButton';
 import Add from '@material-ui/icons/Add';
 import Card from "@material-ui/core/Card";
+import ShoppingCart from '@material-ui/icons/ShoppingCart';
 import Snackbar from '@material-ui/core/Snackbar';
 import CloseIcon from '@material-ui/icons/Close';
 import { CardContent } from '@material-ui/core';
 import CardActions from '@material-ui/core/CardActions';
 import Button from '@material-ui/core/Button';
+import Badge from '@material-ui/core/Badge';
 
 const styles = {
     star: {
@@ -24,14 +26,14 @@ const styles = {
     icon: {
         margin: 10
     },
-      card: {
+    card: {
         width: '90%',
         padding: 10,
         height: 'auto'
     },
     button :{
         width : '100%'
-    }
+    },
 };
 
 class Details extends Component {
@@ -43,7 +45,9 @@ class Details extends Component {
             locality: "",
             categoriesList: [],
             open: false,
-            successMessage: ""
+            successMessage: "",
+            totalNumberOfItems: 0,
+            totalPrice: 0
         }
     }
 
@@ -175,6 +179,7 @@ class Details extends Component {
                                             <IconButton onClick={this.addButtonHandler.bind(this,item.item_name)}>
                                                  <Add />
                                             </IconButton>
+                                            
                                         </div>
                                     </div>
                                  ))
@@ -186,7 +191,19 @@ class Details extends Component {
                      <div className="cart-container">
                          <Card className={classes.card}>
                             <CardContent>
-                                
+                                <div>
+                                  <Badge 
+                                         badgeContent={this.state.totalNumberOfItems} 
+                                         color="primary"
+                                         invisible={false}>
+                                     <ShoppingCart/>
+                                  </Badge> 
+                                  <span className="my-cart"> My Cart</span>
+                                </div>
+                                <div className="total-amount-section">
+                                    <span> TOTAL AMOUNT </span>
+                                    <span> {'\u20B9' + parseFloat(Math.round(this.state.totalNumberOfItems * 100) / 100).toFixed(2)} </span>
+                                </div>
                             </CardContent>
                             <CardActions>
                                 <Button variant="contained" color="primary" className={classes.button}>
